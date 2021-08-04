646a8b1f17a74f0a5ef3bc11169720966cb11291
Pauline Tannos <ptannos2@gmail.com>
2021-08-04 18:35:56 -0400

    {feature}: processed csv file

--- benchmarking/README.md
+++ benchmarking/README.md
@@ -1,7 +1,3 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Available Scripts
 
 In the project directory, you can run:
@@ -10,9 +6,6 @@ In the project directory, you can run:
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
 ### `npm test`
 
--- benchmarking/src/App.js
+++ benchmarking/src/App.js
@@ -1,33 +1,19 @@
 // import "./App.css";
 import React, { useState } from "react";
+import data from "./data";
 
-const dummyData = [
-  {
-    candidate_id: 889,
-    communication_score: 114028,
-    coding_score: 180944,
-    title: "Engineer",
-    company_id: 2,
-  },
-  {
-    candidate_id: 890,
-    communication_score: 62734,
-    coding_score: 64000,
-    title: "Engineer",
-    company_id: 2,
-  },
-];
+console.log(data);
 
 const App = () => {
   const [id, setId] = useState(0);
   const [engineer, setEngineer] = useState({});
   const [percentile, setPercentile] = useState(0);
 
-  console.log(dummyData);
+  const { candidate_id, communication_score, coding_score, title, company_id } =
+    engineer;
 
   // As user starts typing, update ID on state
   const handleChange = (evt) => {
-    console.log(typeof evt.target.value);
     setId(evt.target.value);
   };
 
@@ -35,14 +21,23 @@ const App = () => {
   const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    // Find engineer by ID
-    for (let i = 0; i < dummyData.length; i++) {
-      if (String(dummyData[i].candidate_id) === id) {
-        setEngineer(dummyData[i]);
+    // Find engineer by ID, set state
+    for (let i = 1; i < data.length; i++) {
+      if (String(data[i].candidate_id) === id) {
+        setEngineer(data[i]);
       }
     }
-  };
 
+    // Calculate percentile: number of values below "x" / total number of values * 100
+    let totalLower = 0;
+    for (let i = 1; i < data.length; i++) {
+      if (Number(data[i].coding_score) < Number(engineer.coding_score)) {
+        totalLower += 1;
+      }
+      setPercentile((totalLower / data.length) * 100);
+    }
+  };
+  console.log("percentile", percentile);
   return (
     <div className="App">
       <header className="App-header">
@@ -56,6 +51,12 @@ const App = () => {
           <br />
           <button type="submit">Submit</button>
         </form>
+      </div>
+      <div className="result">
+        Engineer ID#{candidate_id} has a coding score of {coding_score}, a
+        communication score of {communication_score}, and is in the{" "}
+        {Math.round(percentile)}
+        th percentile!
       </div>
     </div>
   );
