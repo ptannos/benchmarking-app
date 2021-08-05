904cb624cd6b4488360dceb95d0ca6d6d748969b
Pauline Tannos <ptannos2@gmail.com>
2021-08-04 20:18:28 -0400

    {feature}: communication percentile calculated

--- benchmarking/src/App.js
+++ benchmarking/src/App.js
@@ -1,5 +1,5 @@
 // import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "./data";
 
 console.log(data);
@@ -7,43 +7,61 @@ console.log(data);
 const App = () => {
   const [id, setId] = useState(0);
   const [engineer, setEngineer] = useState({});
-  const [percentile, setPercentile] = useState(0);
+  const [exist, setExist] = useState(true);
+  const [codingPercentile, setCodingPercentile] = useState(0);
+  const [communicationPercentile, setCommunicationPercentile] = useState(0);
 
-  const { candidate_id, communication_score, coding_score, title, company_id } =
-    engineer;
+  const { candidate_id } = engineer;
 
-  // As user starts typing, update ID on state
-  const handleChange = (evt) => {
-    setId(evt.target.value);
-  };
-
-  // As user submits, look up engineer by ID and return their percentile
-  const calculatePercentile = (engineer) => {
-    let lowerScoreSameTitle = 0;
+  const calculatePercentile = async (engineer) => {
+    let lowerCodingScore = 0;
+    let lowerCommunicationScore = 0;
     let sameTitles = [];
+    // Find other engineers with the same title
     for (let i = 1; i < data.length; i++) {
       if (data[i].title === engineer.title) {
         sameTitles.push(data[i]);
+        // Find other engineers, same title, lower scores
         if (Number(data[i].coding_score) < Number(engineer.coding_score)) {
-          lowerScoreSameTitle += 1;
+          lowerCodingScore += 1;
+        }
+        if (
+          Number(data[i].communication_score) <
+          Number(engineer.communication_score)
+        ) {
+          lowerCommunicationScore += 1;
         }
       }
-      // Calculate percentile: number of values below "x" / total number of values * 100
-      setPercentile((lowerScoreSameTitle / sameTitles.length) * 100);
+      // Percentile = number of values below "x" / total number of values * 100
+      await setCodingPercentile((lowerCodingScore / sameTitles.length) * 100);
+      await setCommunicationPercentile(
+        (lowerCommunicationScore / sameTitles.length) * 100
+      );
     }
-    console.log(lowerScoreSameTitle);
   };
 
+  // Recalculates percentiles every time engineer changes
+  useEffect(() => {
+    calculatePercentile(engineer);
+  }, [engineer]);
+
+  // As user starts typing, update ID on state
+  const handleChange = async (evt) => {
+    await setId(evt.target.value);
+  };
+
+  // When form is submitted, find engineer by ID
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-
-    // Find engineer by ID, set state
     for (let i = 1; i < data.length; i++) {
       if (String(data[i].candidate_id) === id) {
         await setEngineer(data[i]);
+        await setExist(true);
+        return;
       }
     }
-    calculatePercentile(engineer);
+    // If no engineer is found, set exist to false
+    await setExist(false);
   };
 
   return (
@@ -61,18 +79,19 @@ const App = () => {
         </form>
       </div>
       {/* If an engineer with the ID is found, return results */}
-      {Object.keys(engineer).length !== 0 ? (
+      {exist === true && Object.keys(engineer).length !== 0 ? (
         <div className="result">
           Engineer ID#{candidate_id}
           <br />
-          Coding Skills: {Math.round(percentile)}
-          th percentile!
+          Coding Skills: {Math.round(codingPercentile)}
+          th percentile
           <br />
-          Communication Skills:
+          Communication Skills: {Math.round(communicationPercentile)}th
+          percentile
         </div>
-      ) : (
-        <p>Please input a valid ID.</p>
-      )}
+      ) : null}
+
+      {exist === false ? <p>Please input a valid ID.</p> : null}
     </div>
   );
 };
