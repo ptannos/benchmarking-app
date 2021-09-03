b2c1d78f52fa203700e1d6ba8310fb13b399b7d9
Pauline Tannos <ptannos2@gmail.com>
2021-09-03 11:07:53 -0400

    updated file structure

---
+++ benchmarking-app/src/App.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import { allEngineers, allCompanies } from "./datasources/data";
+
+const App = () => {
+  const [id, setId] = useState(0);
+  const [engineer, setEngineer] = useState({});
+  const [exist, setExist] = useState(true);
+  const [codingPercentile, setCodingPercentile] = useState(0);
+  const [communicationPercentile, setCommunicationPercentile] = useState(0);
+
+  const calculatePercentile = async (candidate_id) => {
+    let currEngineer = allEngineers.filter(
+      (obj) => obj.candidate_id === candidate_id
+    )[0];
+
+    // Check if 2 companies are similar
+    const areSimilar = (id1, id2) => {
+      const companyOne = allCompanies.filter((obj) => {
+        return obj.company_id === id1;
+      });
+      const companyTwo = allCompanies.filter((obj) => {
+        return obj.company_id === id2;
+      });
+      return (
+        Math.abs(
+          Number(companyOne[0]["fractal_index"]) -
+            Number(companyTwo[0]["fractal_index"])
+        ) < 0.15
+      );
+    };
+
+    // Find other engineers with same title, at other similar companies
+    let similarEngineers = allEngineers.filter(
+      (engineer) =>
+        currEngineer &&
+        engineer.title === currEngineer.title &&
+        engineer.company_id !== currEngineer.company_id &&
+        areSimilar(engineer.company_id, currEngineer.company_id)
+    );
+
+    // Total scores that are lower
+    let lowerCodingScore = 0;
+    let lowerCommunicationScore = 0;
+    for (let i = 0; i < similarEngineers.length; i++) {
+      if (
+        Number(similarEngineers[i].coding_score) <
+        Number(currEngineer.coding_score)
+      ) {
+        lowerCodingScore += 1;
+      }
+      if (
+        Number(similarEngineers[i].communication_score) <
+        Number(currEngineer.communication_score)
+      ) {
+        lowerCommunicationScore += 1;
+      }
+    }
+
+    // Percentile = number of values below "x" / total number of values * 100
+    await setCodingPercentile(
+      (lowerCodingScore / similarEngineers.length) * 100
+    );
+    await setCommunicationPercentile(
+      (lowerCommunicationScore / similarEngineers.length) * 100
+    );
+  };
+
+  // Recalculates percentiles every time engineer changes
+  useEffect(() => {
+    calculatePercentile(engineer.candidate_id);
+  }, [engineer]);
+
+  // As user starts typing, update ID on state
+  const handleChange = async (evt) => {
+    await setId(evt.target.value);
+  };
+
+  // When form is submitted, find engineer by ID
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    for (let i = 1; i < allEngineers.length; i++) {
+      if (String(allEngineers[i].candidate_id) === id) {
+        await setEngineer(allEngineers[i]);
+        await setExist(true);
+        return;
+      }
+    }
+    // If no engineer is found, set exist to false
+    await setExist(false);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Benchmarking App</p>
+      </header>
+      <div className="form">
+        <form id="App-form" onSubmit={handleSubmit}>
+          <label htmlFor="id">Enter Engineer's ID:</label>
+          <br />
+          <input name="id" onChange={handleChange} />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      {/* If an engineer with the ID is found, return results */}
+      {exist === true && Object.keys(engineer).length !== 0 ? (
+        <div className="result">
+          <p>
+            Coding Skills: {Math.round(codingPercentile)}
+            th percentile
+            <br />
+            Communication Skills: {Math.round(communicationPercentile)}th
+            percentile
+          </p>
+        </div>
+      ) : null}
+      {/* If engineer does not exist, display a helpful message */}
+      {exist === false ? <p>Please input a valid ID.</p> : null}
+    </div>
+  );
+};
+
+export default App;
